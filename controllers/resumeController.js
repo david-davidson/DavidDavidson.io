56d5bdfc130d5c5e0f643148e9c9d5b46806284d
david-davidson <david.davidson@logos.com>
2014-04-21 10:22:34 -0700

    Added function that checks for even- and odd-numbered list items

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -2,5 +2,13 @@ Portfolio.controller('resumeController',
     function ($scope, resumeModel) {
         $scope.jobs = resumeModel.getJobs();
         $scope.schools = resumeModel.getSchools();
+        $scope.isEven = function(index) {
+        if (index % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        };
+    };
     }
 );
