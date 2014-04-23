9922661bb1c651a82f1928eade9e4642fa5c94e5
david-davidson <david.davidson@logos.com>
2014-04-23 08:43:04 -0700

    Set indentation to 4 spaces

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // Set up routing
 var portfolioConfig = function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    controller: 'homeController',
-    templateUrl: 'views/homeView.html',
-  })
-  .when('/resume', {
-    controller: 'resumeController',
-    templateUrl: 'views/resumeView.html',
-  })
-  .when('/portfolio', {
-    controller: 'portfolioController',
-    templateUrl: 'views/portfolioView.html',
-  })
-  .when('/contact', {
-    templateUrl: 'views/contactView.html',
-  })
-  .otherwise({
-    redirectTo: '/'
-  })
-  ;
+    $routeProvider
+    .when('/', {
+        controller: 'homeController',
+        templateUrl: 'views/homeView.html',
+    })
+    .when('/resume', {
+        controller: 'resumeController',
+        templateUrl: 'views/resumeView.html',
+    })
+    .when('/portfolio', {
+        controller: 'portfolioController',
+        templateUrl: 'views/portfolioView.html',
+    })
+    .when('/contact', {
+        templateUrl: 'views/contactView.html',
+    })
+    .otherwise({
+        redirectTo: '/'
+    })
+    ;
 };
 
 // Set up the Portfolio namespace
@@ -27,14 +27,14 @@ var Portfolio = angular.module('Portfolio', []).config(portfolioConfig);
 
 // Set up bullet-points directive
 Portfolio.directive('bullets', function() {
-  return {
-    templateUrl: 'views/bulletsDirective.html'
-  };
+    return {
+        templateUrl: 'views/bulletsDirective.html'
+    };
 });
 
 // Set up the slider directive
 Portfolio.directive('slider', function() {
-  return {
-    templateUrl: 'views/sliderDirective.html'
-  };
+    return {
+        templateUrl: 'views/sliderDirective.html'
+    };
 });
