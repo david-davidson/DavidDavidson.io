01b30f368d6d73105acc3576c1cf225165e7fe4a
David Davidson <davidd@zippercomputer.com>
2014-04-22 19:08:00 -0700

    Moved slider JS into a controller

--- app.js
+++ app.js
@@ -31,48 +31,10 @@ Portfolio.directive('bullets', function() {
     templateUrl: 'views/bulletsView.html'
   };
 });
+
 // Set up the slider directive
-Portfolio.directive('slider', function($timeout) {
+Portfolio.directive('slider', function() {
   return {
-    restrict: 'AE',
-    replace: true,
-    scope: {
-      testimonials: '='
-    },
-    link: function(scope, elem, attrs) {
-      // Start on the first slider, and then increment from there
-      scope.currentIndex = 0; 
-      scope.next = function() {
-        if (scope.currentIndex == scope.testimonials.length - 1) {
-          scope.currentIndex = 0;
-        }
-        else {
-          scope.currentIndex++;
-        }
-      };
-      // Hide every testimonial
-      scope.$watch('currentIndex', function() {
-        scope.testimonials.forEach(function(item) {
-          item.visible = false; 
-        });
-        // Then show the current testimonial
-        scope.testimonials[scope.currentIndex].visible = true; 
-      });
-      scope.activate = function(index) {
-        scope.currentIndex = index;
-      };
-      var timer;
-      var sliderFunc = function() {
-        timer = $timeout(function() {
-          scope.next();
-          timer = $timeout(sliderFunc, 0);
-        }, 15000);
-      };
-      sliderFunc();
-      scope.$on('$destroy', function() {
-        $timeout.cancel(timer);
-      });
-    },
     templateUrl: 'views/sliderView.html'
   };
 });
