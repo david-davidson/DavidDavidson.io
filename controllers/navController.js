ab812d91fa3ad44a82c7b26eb54b0774d92476a6
david-davidson <david.davidson@logos.com>
2014-05-23 08:55:46 -0700

    Added a 750ms timeout when hiding the nav dropdown on ng-mouseout

--- controllers/navController.js
+++ controllers/navController.js
@@ -1,6 +1,6 @@
 Portfolio.controller('navController',
-    ['$scope', '$location', '$routeParams',
-    function ($scope, $location, $routeParams) {
+    ['$scope', '$location', '$timeout',
+    function ($scope, $location, $timeout) {
         $scope.$on('$routeChangeSuccess', function () {
             var currentPage = $location.path();
             if (currentPage == '/resume') {
@@ -24,6 +24,20 @@ Portfolio.controller('navController',
                 $scope.title = 'David Davidson | web development, copywriting, marketing';
             }
         });
+        // On its own, ng-mouseout fires closeDropDown() way too readily, so we add one more boolean: $scope.dropdownHover, which (on mouseout) we 1. set to false *before* the timeout and 2. then check again before actually hiding the dropdown
+        $scope.hideDropdown = function() {
+            if ($scope.dropdownHover === false) {
+                $scope.dropdown = false;
+            }
+        };
+        $scope.closeDropdown = function() {
+            $scope.dropdownHover = false;
+            $timeout($scope.hideDropdown, 750); // 750ms delay after leaving the dropdown
+        };
+        $scope.openDropdown = function() {
+            $scope.dropdown = true;
+            $scope.dropdownHover = true;
+        };
     }
 ]);
 // navController.$inject = ['$scope', '$location'];
