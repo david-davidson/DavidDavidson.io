ff4d68b8930db6bb21448fb9051c8c6db85c3b8e
david-davidson <david.davidson@logos.com>
2014-06-08 10:20:09 -0700

    Cleaned up formatting and commenting, and moved filterNewProjects() into its own function

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,24 +1,28 @@
 Portfolio.controller('portfolioController',
-    ['$scope', '$window', 'portfolioModel',
-    function ($scope, $window, portfolioModel) {
+    ['$scope', 'portfolioModel',
+    function ($scope, portfolioModel) {
         $scope.projects = portfolioModel.getProjects();
         $scope.filteredProjects = $scope.projects;
         $scope.copy = portfolioModel.getCopy();
         $scope.$watch('technology', function() {
-            if (typeof $scope.technology != 'undefined') {
-                $scope.filteredProjects = [];
+            function filterNewProjects() {
+                newProjects = [];
                 angular.forEach($scope.projects, function(project) { 
                     found = false;
                     angular.forEach(project.technologies, function(technologies) {
-                        if (technologies.bullet.toLowerCase().indexOf($scope.technology.toLowerCase()) != -1) {
+                        if (technologies.bullet.toLowerCase().search($scope.technology.toLowerCase()) != -1) {
                             found = true;
                         }
                     });
                     if (found === true) {
-                        $scope.filteredProjects.push(project);
-                        return true;
+                        newProjects.push(project);
                     }
                 });
+                return newProjects;
+            }
+            // Don't filter when there's nothing to filter against, just to keep the console happy:
+            if (typeof $scope.technology != 'undefined') {
+                $scope.filteredProjects = filterNewProjects(); // To avoid a flicker on slow connections, we build the new array behind the scenes and then switch over all at once, rather than emptying and rebuilding it while it's live in the view
             }
         });
         $scope.noContentVisible = function() {
@@ -28,6 +32,5 @@ Portfolio.controller('portfolioController',
                 return false;
             }
         };
-        $scope.$on('$viewContentLoaded', setHeight());
     }
 ]);
