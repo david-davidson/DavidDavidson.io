de2ef905c6f7c5eb9d3a099937ca793ee457a83b
David Davidson <daviddavidson.1988@gmail.com>
2014-09-10 16:48:36 -0700

    Add JS code style task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,28 @@
-module.exports = function(grunt){
-    "use strict";
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+module.exports = function(grunt) {
+    'use strict';
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        // Check code
+        jshint: {
+            files: [
+                'server.js',
+                'app/app.js',
+                'app/models/*.js',
+                'app/controllers/*.js'
+            ]
+        },
+        jscs: {
+            src: [
+                'app/**/*.js',
+                '*.js'
+            ],
+            options: {
+                config: '.jscsrc'
+            }
+        },
+
+        // Build
         clean: {
             all: {
                 src: [
@@ -25,6 +45,48 @@ module.exports = function(grunt){
                 filter: 'isFile'
             }
         },
+        sass: {
+            build: {
+                files: {
+                    'dist/styles.css': 'app/views/styles/styles.scss'
+                }
+            }
+        },
+        cssmin: {
+            build: {
+                src: 'dist/styles.css',
+                dest: 'dist/styles.css'
+            }
+        },
+        concat: {
+            all: {
+                src: [
+                    'app/app.js',
+                    'app/models/*.js',
+                    'app/controllers/*.js'
+                ],
+                dest: 'dist/scripts.js'
+            }
+        },
+        uglify: {
+            all: {
+                files: {
+                    'dist/scripts.js': [
+                        'dist/scripts.js'
+                    ]
+                }
+            }
+        },
+
+        // Serve and watch
+        express: {
+            dev: {
+                options: {
+                    script: 'server.js',
+                    background: true // Runs in the background, so we can set up a watch task!
+                }
+            }
+        },
         watch: {
             all: {
                 files: [
@@ -38,64 +100,28 @@ module.exports = function(grunt){
                 ],
                 tasks: [
                     'jshint',
+                    'jscs',
                     'clean',
                     'copy',
                     'sass',
                     'cssmin',
                     'concat',
-                    'uglify'
-                ],
-                options: {
-                    livereload: false // true if using express/open
-                }
-            }
-        },
-        jshint: {
-            files: [
-                'server.js',
-                'app/app.js',
-                'app/models/*.js',
-                'app/controllers/*.js'
-            ]
-        },
-        concat: {
-            all: {
-                src: [
-                    'app/app.js',
-                    'app/models/*.js',
-                    'app/controllers/*.js'],
-                dest: 'dist/scripts.js'
-            }
-        },
-        uglify: {
-            all: {
-                files: {
-                    'dist/scripts.js': ['dist/scripts.js']
-                }
-            }
-        },
-        sass: {
-            build: {
-                files: {
-                    'dist/styles.css': 'app/views/styles/styles.scss'
-                }
-            }
-        },
-        cssmin: {
-            build: {
-                src: 'dist/styles.css',
-                dest: 'dist/styles.css'
+                    'uglify',
+                    'express:dev'
+                ]
             }
         }
     });
 grunt.registerTask('default', [
     'jshint',
+    'jscs',
     'clean',
     'copy',
     'sass',
     'cssmin',
     'concat',
     'uglify',
+    'express:dev',
     'watch'
     ]);
 };
