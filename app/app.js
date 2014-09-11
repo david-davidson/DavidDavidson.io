7a0ba3a8a1c4c6cbff62c6c5d1122affbd53367e
David Davidson <daviddavidson.1988@gmail.com>
2014-09-11 08:09:02 -0700

    Adopt more modular architecture, with 'require' to bring in Angular, controllers, etc.

--- app/app.js
+++ app/app.js
@@ -1,55 +1,60 @@
-var portfolioConfig,
-    Portfolio,
+'use strict';
 
-// Set up routing
-portfolioConfig = function($routeProvider, $locationProvider) {
-    $routeProvider
-    .when('/', {
-        controller: 'homeController',
-        templateUrl: 'views/homeView.html'
-    })
-    .when('/resume', {
-        controller: 'resumeController',
-        templateUrl: 'views/resumeView.html'
-    })
-    .when('/copy', {
-        controller: 'portfolioController',
-        templateUrl: 'views/copyView.html'
-    })
-    .when('/portfolio', {
-        controller: 'portfolioController',
-        templateUrl: 'views/portfolioView.html'
-    })
-    .when('/contact', {
-        controller: 'contactController',
-        templateUrl: 'views/contactView.html'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
-    $locationProvider.html5Mode(true);
-};
+(function() {
+    require('angular/angular');
+    require('angular-route');
 
-// Set up the Portfolio namespace
-Portfolio = angular.module('Portfolio', []).config([
-    '$routeProvider', '$locationProvider',
-    portfolioConfig ]);
+    var Portfolio = angular.module('Portfolio', [
+            'ngRoute'
+        ]);
 
-// Directives
+    // Models
+    require('./models/homeModel')(Portfolio);
+    require('./models/portfolioModel')(Portfolio);
+    require('./models/resumeModel')(Portfolio);
 
-// Down arrows
-Portfolio.directive('ddArrow', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'views/directives/arrow.html'
-    };
-});
+    // Controllers
+    require('./controllers/masterController')(Portfolio);
+    require('./controllers/contactController')(Portfolio);
+    require('./controllers/homeController')(Portfolio);
+    require('./controllers/portfolioController')(Portfolio);
+    require('./controllers/resumeController')(Portfolio);
+    require('./controllers/sliderController')(Portfolio);
 
-// Slider
-Portfolio.directive('ddSlider', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'views/directives/slider.html',
-        controller: 'sliderController'
-    };
-});
+    // Filters
+    require('./filters/toHTML')(Portfolio);
+
+    // Directives
+    require('./directives/arrow')(Portfolio);
+    require('./directives/slider')(Portfolio);
+
+    // Routes
+    Portfolio.config([ '$routeProvider', function($routeProvider) {
+        $routeProvider
+        .when('/', {
+            controller: 'homeController',
+            templateUrl: 'views/homeView.html'
+        })
+        .when('/resume', {
+            controller: 'resumeController',
+            templateUrl: 'views/resumeView.html'
+        })
+        .when('/copy', {
+            controller: 'portfolioController',
+            templateUrl: 'views/copyView.html'
+        })
+        .when('/portfolio', {
+            controller: 'portfolioController',
+            templateUrl: 'views/portfolioView.html'
+        })
+        .when('/contact', {
+            controller: 'contactController',
+            templateUrl: 'views/contactView.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+        // $locationProvider.html5Mode(true);
+    } ]);
+
+}());
