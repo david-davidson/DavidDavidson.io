99be84a3e6f2e0004be574121ed52dd7f53a01c5
David Davidson <daviddavidson.1988@gmail.com>
2014-10-23 16:27:28 -0700

    Move the filterProjects function onto $scope, to make it available for testing

--- app/controllers/portfolioController.js
+++ app/controllers/portfolioController.js
@@ -8,28 +8,30 @@ module.exports = function(app) {
             $scope.filteredProjects = $scope.projects;
             $scope.copy = portfolioModel.getCopy();
 
-            $scope.$watch('technology', function() {
-                 // To avoid a flicker on slow connections, we build the new array behind the scenes and then switch over all at once, rather than emptying and rebuilding it while it's live in the view.
-                var filterNewProjects = function() {
-                    var newProjects = [];
+            // To avoid a flicker on slow connections, we build the new array behind the scenes and then switch over all at once, rather than emptying and rebuilding it while it's live in the view.
+            $scope.filterProjects = function(projects, inputTechnology) {
+                
+                var newProjects = [];
 
-                    angular.forEach($scope.projects, function(project) {
-                        var found = false;
-                        // Since we only need one match, let's stop looping after we find it: some, not forEach
-                        found = project.technologies.some(function(technologies) {
-                            return (technologies.bullet.toLowerCase().indexOf($scope.technology.toLowerCase()) != -1);
-                        });
-
-                        if (found) {
-                            newProjects.push(project);
-                        }
+                angular.forEach(projects, function(project) {
+                    
+                    var found = false;
+                    // Since we only need one match, let's stop looping after we find it: some, not forEach
+                    found = project.technologies.some(function(technologies) {
+                        return (technologies.bullet.toLowerCase().indexOf(inputTechnology.toLowerCase()) != -1);
                     });
-                    return newProjects;
-                };
+                    if (found) {
+                        newProjects.push(project);
+                    }
 
+                });
+                return newProjects;
+            };
+
+            $scope.$watch('technology', function() {
                 // Don't filter when there's nothing to filter against, just to keep the console happy:
-                if (typeof $scope.technology != 'undefined') {
-                    $scope.filteredProjects = filterNewProjects();
+                if (typeof $scope.technology !== 'undefined') {
+                    $scope.filteredProjects = $scope.filterProjects($scope.projects, $scope.technology);
                 }
             });
 
