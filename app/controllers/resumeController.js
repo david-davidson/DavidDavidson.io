c44533bf7ba10477ec59bb6aedcf8178d21bc4f4
David Davidson <daviddavidson.1988@gmail.com>
2014-10-23 15:33:24 -0700

    Remove unnecessary isEven function

--- app/controllers/resumeController.js
+++ app/controllers/resumeController.js
@@ -6,15 +6,6 @@ module.exports = function(app) {
         function($scope, resumeModel) {
             $scope.jobs = resumeModel.getJobs();
             $scope.schools = resumeModel.getSchools();
-
-            $scope.isEven = function(index) {
-                if (index % 2 === 0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            };
-
         }
     ]);
 };
