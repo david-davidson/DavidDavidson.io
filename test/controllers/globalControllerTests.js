2983efe70f23745c365ad92b3b6c68ccafb0b4d7
David Davidson <daviddavidson.1988@gmail.com>
2014-11-14 11:14:03 -0800

    Compare date string, not date objects

--- test/controllers/globalControllerTests.js
+++ test/controllers/globalControllerTests.js
@@ -33,7 +33,7 @@ describe('The global controller', function() {
 	});
 
 	it('Exposes the current date, for use in the footer', function() {
-		expect(scope.today).toEqual(new Date());
+		expect(scope.today.toDateString()).toEqual(new Date().toDateString());
 	});
 
 	it('Lets the UI open the portfolio dropdown', function() {
